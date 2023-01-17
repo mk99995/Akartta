b2888a45a9577ab1e6d9ce98e060d2d55fa5a81e
mk99995 <mejkorte@gmail.com>
2023-01-17 13:55:07 +0200

    added timetable

---
+++ src/components/TimeTable/Hour.tsx
@@ -0,0 +1,13 @@
+import "./TimeTable.scss";
+
+const Hour = ({ hour }: { hour: number }): JSX.Element => {
+  return (
+    <tbody>
+      <tr>
+        <td>asd</td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default Hour;
--- src/components/TimeTable/TimeTable.tsx
+++ src/components/TimeTable/TimeTable.tsx
@@ -1,7 +1,54 @@
 import "./TimeTable.scss";
+import Hour from "./Hour";
+import { useState } from "react";
+import { a } from "@tauri-apps/api/dialog-15855a2f";
 
 const TimeTable = (): JSX.Element => {
-  return <div className="TimeTable window"></div>;
+  const [hourList, setHourList] = useState<number[]>([]);
+  const renderTimeTable = (start: number, hours: number): JSX.Element[] => {
+    let timeTable: JSX.Element[] = [];
+    if (start >= hours) {
+      return [<></>];
+    }
+    for (let i = start; i < hours; i++) {
+      let odd: string = "";
+
+      if (i % 2 !== 0) {
+        odd = " odd";
+      }
+
+      timeTable.push(
+        <>
+          <tr>
+            <td className="number">{}</td>
+            <td className={`first${odd}`}>{}</td>
+          </tr>
+
+          <tr>
+            <td className="number half-hour">{i + ":30"}</td>
+            <td className={`second${odd}`}>{}</td>
+          </tr>
+          <tr>
+            <td className="number"></td>
+            <td className={`third${odd}`}>{}</td>
+          </tr>
+          <tr>
+            <td className="number">{i === hours - 1 ? "" : i + 1 + ":00"}</td>
+            <td className={`fourth${odd}`}>{}</td>
+          </tr>
+        </>
+      );
+    }
+    return timeTable;
+  };
+
+  return (
+    <div className="TimeTable window">
+      <table>
+        <tbody>{renderTimeTable(5, 24)}</tbody>
+      </table>
+    </div>
+  );
 };
 
 export default TimeTable;
